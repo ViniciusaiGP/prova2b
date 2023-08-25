3f7eb0bf80a07e70a99e88983a1d9b00dfab2b06
Vinícius Mota Zulianelli <70486496+ViniciusaiGP@users.noreply.github.com>
2023-08-24 21:46:04 -0300

    Projeto completo

--- app/src/main/java/com/example/prova2b/MainActivity.java
+++ app/src/main/java/com/example/prova2b/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.prova2b;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button form, list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        form = findViewById(R.id.form);
+        list = findViewById(R.id.list);
+        form.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent form = new Intent(MainActivity.this, FormActivity.class);
+                startActivity(form);
+            }
+        });
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent list = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(list);
+            }
+        });
     }
 }
